bf5085ebf7cf3d2c94749c3012c781d2a8889692
carlos-heins <57270985+carlos-heins@users.noreply.github.com>
2022-05-05 14:51:34 -0300

    examples

---
+++ validation-composite.ts
@@ -0,0 +1,53 @@
+// Component
+export abstract class ValidationComponent {
+  abstract validate(value: unknown): boolean;
+}
+
+// Leaf
+export class ValidateEmail extends ValidationComponent {
+  validate(value: unknown): boolean {
+    if (typeof value !== 'string') return false;
+    return /@/.test(value);
+  }
+}
+
+export class ValidateString extends ValidationComponent {
+  validate(value: unknown): boolean {
+    return typeof value === 'string';
+  }
+}
+
+export class ValidateNumber extends ValidationComponent {
+  validate(value: unknown): boolean {
+    if (typeof value !== 'string') return false;
+    return /\d+/.test(value);
+  }
+}
+
+// Composite
+export class ValidationComposite extends ValidationComponent {
+  private readonly children: ValidationComponent[] = [];
+
+  validate(value: unknown): boolean {
+    for (const child of this.children) {
+      const validation = child.validate(value);
+      if (!validation) return false;
+    }
+    return true;
+  }
+
+  add(...validations: ValidationComponent[]): void {
+    validations.forEach((validation) => this.children.push(validation));
+  }
+}
+
+const validateEmail = new ValidateEmail();
+const validateNumber = new ValidateNumber();
+const validateString = new ValidateString();
+const validationComposite = new ValidationComposite();
+validationComposite.add(validateString, validateEmail, validateNumber);
+console.log(validationComposite.validate('CarlinhosDaVan123@email.com'));
+
+
+// nesse exemplo da internet a gente basicamente cria algumas validações e usa o validationComposite para usar todas no testar
+// se a string passada é um email. 
